a386b2d77a4d3d91c92e32f504dcded89ba1b1b9
Fangliang Wang <Fangliang Wang>
2016-11-08 23:48:41 +0800

    ch13 complete to resolve empty and repeat

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,7 +6,11 @@ from lists.views import home_page
 from lists.models import Item
 from lists.models import List
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import (
+    EMPTY_LIST_ERROR, ItemForm,
+    DUPLICATE_ITEM_ERROR, ExistingListItemForm,
+)
+from unittest import skip
 
 # Create your tests here.
 
@@ -88,7 +92,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         print(response.content.decode())
         self.assertContains(response, 'name="text"')
 
@@ -110,11 +114,25 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text':'textey'}
+        )
+
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+        
 
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
